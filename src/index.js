f55a744dc2fe22d52d9cdc2d40124c681c969d74
Angela Hennig <angela.hennigx@gmail.com>
2026-04-20 20:26:27 -0300

    Corrigindo erro de sintaxe e finalizando Dockerfile

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-console.log('Iniciando projeto...');
-console.log('Finalizado.');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Petshop DevOps rodando no Docker!\n');
+});
+
+server.listen(8080, () => {
+  console.log('Servidor rodando na porta 8080...');
+});
